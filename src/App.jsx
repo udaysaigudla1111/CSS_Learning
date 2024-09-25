8b8caf36fe31240803cd766f8dc237818cbece70
GUDLA UDAY SAI <udaysaigudla1111@gmail.com>
2024-09-26 00:32:02 +0530

    Step-1 completed the selection concept

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Component1 from './Component1'
+
+const App = () => {
+  return (
+  <>
+    <Component1 />
+  </>
+  )
+}
+
+export default App
---
+++ src/Component1.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Component1 = () => {
+  return (
+    <>
+    <div className='flex justify-center mt-11'>
+        GFG course
+    </div>
+    <div className=' h-screen flex items-center justify-center selection:bg-green-700 selection:text-white'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
+    </>
+  )
+}
+
+export default Component1
